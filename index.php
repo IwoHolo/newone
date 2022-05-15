3acf904874b34ad1c02f96dc0e7cc57a09deecfa
IwoHolo <99130986+IwoHolo@users.noreply.github.com>
2022-05-15 10:25:24 +0200

    new

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+        <link rel="stylesheet" type="text/css" href="css/style2.css" />
+        <meta charset="UTF-8">
+        <title> Historia programowania </title>
+	</head>
+<body background="foto/tło.png">
+    
+<div id="wszystko">
+<?php
+		include 'skrypty/naglowek.php';
+?>
+<div id="cont">
+<?php		
+		include 'skrypty/menu.php';
+		include 'skrypty/menu2.php';
+?>
+</div>
+<?php
+	include 'skrypty/stopka.php';
+?>
+</div>
+</body>
+</html>
